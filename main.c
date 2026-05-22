7d65007a4b500fcd7250a72bb2fea5555580b18f
IbrahimHYoussef <ibrahimhyoussef18@gmail.com>
2026-05-22 03:00:07 +0300

    Add camera, background, and WASD controls

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
+# include <stdio.h>
 # include <stdbool.h>
 # include <raylib.h>
 # include <unistd.h>
 # include <math.h>
 # define RESOUCE_PATH "assets"
 
-//
+// tiled renderer
+typedef struct TiledRenderer{
+    float backgroundSize;
+    Texture2D texture;
+}TiledRenderer;
+
+
+
 
 // Game data
 typedef struct GameData {
@@ -13,16 +21,19 @@ typedef struct GameData {
 
 GameData gameData = {
     .playersPosition={100,100}
-
 };
 
 
 // textures
 Texture2D spaceShip;
+Texture2D backgroud;
+
+// camera
+Camera2D camera = {0};
 
 bool init_game(){
     spaceShip = LoadTexture(RESOUCE_PATH"/spaceShip/ships/green.png");
-    
+    backgroud = LoadTexture(RESOUCE_PATH"/background1.png");
     return true;
 }
 
@@ -36,18 +47,18 @@ Vector2 normalize(Vector2 vector){
 void moveShip(float deltaTime){
     Vector2 move = {.x=0,.y=0};
     // move up
-    if (IsKeyDown(KEY_DOWN)){
+    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)){
         move.y++;
     }
     // move Down
-    if (IsKeyDown(KEY_UP)){
+    if (IsKeyDown(KEY_UP) ||IsKeyDown(KEY_W)){
         move.y--;
     }
     // move Right
-    if (IsKeyDown(KEY_RIGHT)){
+    if (IsKeyDown(KEY_RIGHT)||IsKeyDown(KEY_D)){
         move.x++;
     }
-    if (IsKeyDown(KEY_LEFT)){
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)){
         move.x--;
     }
     if (move.x != 0 || move.y!=0){
@@ -57,8 +68,16 @@ void moveShip(float deltaTime){
         gameData.playersPosition.x += move.x;
         gameData.playersPosition.y += move.y;
     }
+    // update camera
+    camera.target = gameData.playersPosition;
 }
 
+void attachCameraToPlayer(Vector2 vector,float sWidth,float sHight){
+    camera.target = (Vector2){vector.x,vector.y};
+    camera.offset = (Vector2){sWidth/2.0f,sHight/2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+}
 
 bool clear_game(){
     return false ;
@@ -75,22 +94,29 @@ int main (){
         // TODO add message box in here to show to the user
         return 0;
     }
+    float time;
+    #ifdef SHOWFRAME
+    float fps;
+    char fpsC[9];
+    #endif
+    attachCameraToPlayer(gameData.playersPosition, 900, 600);
     while(!WindowShouldClose()){
-        float time = GetFrameTime();
-        BeginDrawing();
+        time = GetFrameTime();
+
         ClearBackground(RAYWHITE);
+        BeginMode2D(camera);
+        DrawTexture(backgroud,0,0,WHITE);
         # pragma region movement
-
         moveShip(time);
-
         # pragma endregion
 
         DrawTexture(spaceShip, gameData.playersPosition.x, gameData.playersPosition.y,WHITE);
-        DrawText("HELLO TO RAY LIB", 10, 20, 50, LIGHTGRAY);
-
+        #ifdef SHOW_FRAME
+        fps = GetFPS();
+        sprintf(fpsC,"%.2f",fps);
+        DrawText(fpsC,10, 10, 50, WHITE);
+        #endif
         EndDrawing();
     }
-
-    // sleep(10);
     return 0;
 }
